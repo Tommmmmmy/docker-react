831540147d46b6f109f9ee72a83427bfbe3b0942
tommytang1995 <tommytang1995@gmail.com>
2020-12-21 20:55:45 -0500

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> anddcd save to reloadere1ds32.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
