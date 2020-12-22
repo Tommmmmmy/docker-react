7595706d8935578fd82aa3f018c4d1612d560634
tommytang1995 <tommytang1995@gmail.com>
2020-12-21 20:28:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> anddcd save to reloadere1ds32.
         </p>
         <a
           className="App-link"
